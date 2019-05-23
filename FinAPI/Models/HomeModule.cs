a295be33b736cad6d5a5a4e8179cdb9684bca8ee
Princebin <727269092@qq.com>
2019-05-23 14:22:14 +0800

    增加简单实例

---
+++ FinAPI/Models/HomeModule.cs
@@ -0,0 +1,14 @@
+﻿using Nancy;
+
+namespace FinAPI.Models
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get("/", r => "Nancy running on ASP.NET Core LineZero");
+            Get("/{name}", r => "简单的路由模板，路由参数：" + r.name);
+            Get("/404", r => HttpStatusCode.NotFound);
+        }
+    }
+}
